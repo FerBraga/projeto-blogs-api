5d977da486446c9688772e06fc22d282d947a283
fer braga <augustofbg@gmail.com>
2022-07-14 19:16:08 -0300

    feat: requisito 1 pronto e corrigido

--- src/database/migrations/3-Categories.js
+++ src/database/migrations/3-Categories.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Categories', {
-      Id: {
+      id: {
         autoIncrement: true,
         primaryKey: true,
         allowNull: false,
